91ed5c1a4281565cb1c5a83c7cc68a1ace2eed3a
Ashley Hufstetler <78829040+Ashley-Elizabeth@users.noreply.github.com>
2021-06-17 01:51:27 -0700

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+var msg = 'hello world';
+console.log(msg);
+
+const kelvin = 283;
+const celsius = kelvin - 273;
+let fahrenheit = celsius * (9/5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
